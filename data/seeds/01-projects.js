43b6734a919dcdfa144773fcde06fe79a4648201
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-14 08:50:05 -0800

    added more seed data for projects and actions

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -5,5 +5,45 @@ exports.seed = function(knex, Promise) {
       description:
         'Build and Awesome API Using Node.js and Express to Manage Projects and Actions GTD Style!',
     },
+    {
+      name: 'Project 2',
+      description:
+        'Description 2',
+    },
+    {
+      name: 'Project 3',
+      description:
+        'Description 3',
+    },
+    {
+      name: 'Project 4',
+      description:
+        'Description 4',
+    },
+    {
+      name: 'Project 5',
+      description:
+        'Description 6',
+    },
+    {
+      name: 'Project 6',
+      description:
+        'Description 6',
+    },
+    {
+      name: 'Project 7',
+      description:
+        'Description 7',
+    },
+    {
+      name: 'Project 8',
+      description:
+        'Description 8',
+    },
+    {
+      name: 'Project 9',
+      description:
+        'Description 9',
+    },
   ]);
 };
