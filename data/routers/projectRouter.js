92c9742c643b91c75284566ced2f5029587f253b
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-14 10:26:06 -0800

    PUT /projects/:id complete

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -25,7 +25,7 @@ router.get("/:id", middleware.validateProjectID, (req, res) => {
 
 });
 
-// GET "/:id/actions"
+// GET "/:id/actions" (retrieves only the actions for a project, without any other data)
 router.get("/:id/actions", middleware.validateProjectID, (req, res) => {
 
     res.status(200).json(req.projectData.actions);
@@ -60,6 +60,24 @@ router.post("/", middleware.validateProjectBody, (req, res) => {
         .catch(error => {
             console.log("POST '/' error:", error);
             res.status(500).json({error: "Couldn't add data to projects database."});
+        })
+
+});
+
+
+// PUT "/:id"
+router.put("/:id", middleware.validateProjectBody, middleware.validateProjectID, (req, res) => {
+
+    console.log("validation passed", req.body)
+
+    database.update(req.projectData.id, req.body)
+        .then(response => {
+                console.log("PUT '/:id':", response);
+                res.status(200).json(response);
+            })
+        .catch(error => {
+            console.log("PUT '/:id' error:", error);
+            res.status(500).json({error: "Couldn't update data in projects database."});
         })
 
 });
